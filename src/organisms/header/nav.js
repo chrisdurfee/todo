797f08af04f6ce65bc6b5e0856144ca1cd492c1e
tech_e <chris.durfee@dentalqore.com>
2024-10-24 15:07:56 -0600

    Update NavLi function: add exact parameter for precise routing

--- src/organisms/header/nav.js
+++ src/organisms/header/nav.js
@@ -8,10 +8,11 @@ import { NavLink } from "@base-framework/base";
  * @param {string} text
  * @returns {object}
  */
-const NavLi = (href, text) => (new NavLink(
+const NavLi = (href, text, exact = false) => (new NavLink(
 {
     href,
     text,
+    exact,
     class: 'inline-flex items-center justify-center whitespace-nowrap rounded-md px-8 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow',
     dataSet: ['selected', ['state', true, 'active']]
 }));
@@ -22,7 +23,7 @@ const NavLi = (href, text) => (new NavLink(
  * @returns {object}
  */
 export const Nav = () => Ul({ class: 'inline-flex h-9 items-center justify-center rounded-lg bg-muted p-1 text-muted-foreground mt-8' }, [
-    NavLi('./', 'All'),
+    NavLi('./', 'All', true),
     NavLi('active', 'Active'),
     NavLi('completed', 'Completed')
 ]);
