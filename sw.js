5b9fa6f4303cba705924a1b3ffcb4dcdb24f2404
chrisdurfee <chrisdurfee@users.noreply.github.com>
2025-01-16 00:17:34 +0000

    deploy: 9771fc520478dfd57586a1e0e121cba02421c4b4

--- sw.js
+++ sw.js
@@ -1,13 +1,38 @@
 importScripts('./worker/cache-controller.js', './worker/service.js');
 
-const appName = 'base-example',
-version = '0.0.2';
+/**
+ * This is the name of the app and the version. This is used to
+ * create the cache name.
+ *
+ * @const
+ * @type {string} appName
+ */
+const APP_NAME = 'base-example',
 
-const files =
+/**
+ * This is the version of the app. This is used to create the cache
+ *
+ * @const
+ * @type {string} version
+ */
+version = '0.0.03';
+
+/**
+ * This will add these files to cache. Add any files you want to
+ * cache here. As files are fetched, they will be added to the cache.
+ *
+ * @const
+ * @type {Array<string>} DEFAULT_FILES
+ */
+const DEFAULT_FILES =
 [
 	'./',
 	'./index.html'
 ];
 
-const appNameId = appName + '-v' + version;
-const service = new Service(appNameId, files);
+/**
+ * This will set up the service worker controller with
+ * the app name and the files to cache.
+ */
+const appNameId = `${APP_NAME}-${version}`;
+const service = new Service(appNameId, DEFAULT_FILES);
